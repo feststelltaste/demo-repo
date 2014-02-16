d4cda79950f4347f1d3fbe71ac4868edf516a73f
Priya Chandran <priya.chandran@example.com>
2014-02-16 00:03:05 +0000

    Refactor service layer

---
+++ src/orders/order_validation.py
@@ -0,0 +1,25 @@
+import re
+
+EMAIL_RE = re.compile(r"^[^@\s]+@[^@\s]+\.[^@\s]+$")
+PHONE_RE = re.compile(r"^\+?[0-9\-\s]{7,15}$")
+
+
+def validate_email(value):
+    if not value:
+        return False
+    return bool(EMAIL_RE.match(value))
+
+
+def validate_phone(value):
+    if not value:
+        return False
+    return bool(PHONE_RE.match(value))
+
+
+def validate_order_input(data):
+    errors = []
+    if not validate_email(data.get("email")):
+        errors.append("invalid email")
+    if not validate_phone(data.get("phone")):
+        errors.append("invalid phone")
+    return errors
