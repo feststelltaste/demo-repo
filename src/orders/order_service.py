8d1bc394a8ae0dc73051109e3cef5393b60b84c3
Casper Voss <casper.voss@example.org>
2014-02-28 22:18:34 +0000

    Improve error handling

---
+++ src/orders/order_service.py
@@ -0,0 +1,8 @@
+from src.orders.order_validation import validate_order_input
+
+
+def place_order(data):
+    errors = validate_order_input(data)
+    if errors:
+        raise ValueError(", ".join(errors))
+    return {"status": "placed", "email": data["email"]}
