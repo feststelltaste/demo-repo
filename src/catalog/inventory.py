71d0aea436c3ca6019f071843fca940b45efd763
tkowalski <tkowalski@example.com>
2014-06-17 20:06:10 +0000

    Clean up unused code

---
+++ src/catalog/inventory.py
@@ -0,0 +1,16 @@
+_stock = {}
+
+
+def set_stock(sku, quantity):
+    _stock[sku] = quantity
+
+
+def get_stock(sku):
+    return _stock.get(sku, 0)
+
+
+def reserve(sku, quantity):
+    available = get_stock(sku)
+    if available < quantity:
+        raise ValueError("not enough stock")
+    _stock[sku] = available - quantity
