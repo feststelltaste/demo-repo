f8bba55db09ae49e85c8d3d385e4ef4f62d9fc8b
Robin Falk <rfalk@example.org>
2014-06-30 23:54:17 +0000

    Fix bug in checkout flow

--- src/catalog/pricing.py
+++ src/catalog/pricing.py
@@ -13,3 +13,5 @@ def apply_discount(net_amount, percentage):
     return round(net_amount * (1 - percentage / 100), 2)
 
 # PROJ-1042: update report for new pricing rules (2014-03-19 08:52:23 +0000)
+
+# Fix bug in checkout flow (2014-06-30 23:54:17 +0000)
