b6112701c6c7c2528b3525ec6c826b5418596a95
Renata Alves <renata.alves@example.net>
2014-09-11 23:28:26 +0000

    Fix null pointer exception

--- src/reports/report_builder.py
+++ src/reports/report_builder.py
@@ -1,3 +1,5 @@
 def build_daily_report(orders):
     total = sum(o.get("amount", 0) for o in orders)
     return {"order_count": len(orders), "total_amount": total}
+
+# Fix null pointer exception (2014-09-11 23:28:26 +0000)
