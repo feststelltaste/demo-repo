723f8c3b2e20b7a227ea235cbd2ac844409ca725
Devon Okafor <devon.okafor@example.org>
2014-09-22 04:08:36 +0000

    Refactor service layer

--- src/orders/order_service.py
+++ src/orders/order_service.py
@@ -8,3 +8,5 @@ def place_order(data):
     return {"status": "placed", "email": data["email"]}
 
 # Clean up unused code (2014-07-20 15:06:56 +0000)
+
+# Refactor service layer (2014-09-22 04:08:36 +0000)
