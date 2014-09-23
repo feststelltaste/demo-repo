7eb3458a3cd93089009d412b4ef8b0de90b77b91
Priya Chandran <priya.chandran@example.com>
2014-09-23 19:15:06 +0000

    Update dependencies

--- src/reports/report_builder.py
+++ src/reports/report_builder.py
@@ -5,3 +5,5 @@ def build_daily_report(orders):
 # Fix null pointer exception (2014-09-11 23:28:26 +0000)
 
 # PROJ-1042: wire up new pricing rules (2014-09-20 17:30:39 +0000)
+
+# Update dependencies (2014-09-23 19:15:06 +0000)
