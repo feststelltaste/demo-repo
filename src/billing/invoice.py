a97043637d10f015d27dba881409bfb14e57e852
priya <priya+192.168.1.162@example.com>
2014-09-25 10:25:29 +0000

    Update documentation

--- src/billing/invoice.py
+++ src/billing/invoice.py
@@ -20,3 +20,5 @@ def build_invoice_line(description, net_amount):
 # Update documentation (2014-07-30 19:47:31 +0000)
 
 # Improve error handling (2014-08-07 18:35:12 +0000)
+
+# Update documentation (2014-09-25 10:25:29 +0000)
