1d52cb54b8b763a669a8b3ecaef15c6f5c4bad2d
Sana Farouk <sana.farouk@example.org>
2014-11-04 16:00:05 +0000

    PROJ-1042: wire up new pricing rules

--- src/billing/invoice.py
+++ src/billing/invoice.py
@@ -24,3 +24,5 @@ def build_invoice_line(description, net_amount):
 # Update documentation (2014-09-25 10:25:29 +0000)
 
 # Clean up unused code (2014-10-14 08:25:13 +0000)
+
+# PROJ-1042: wire up new pricing rules (2014-11-04 16:00:05 +0000)
