4564008f94225ba649728147c43619e9e98ce139
Petra Lindqvist <petra.lindqvist@example.com>
2015-02-22 18:04:34 +0000

    Refactor service layer

--- src/billing/invoice_export.py
+++ src/billing/invoice_export.py
@@ -9,3 +9,5 @@ def export_invoice_as_csv_row(invoice):
     return f"{invoice['description']},{invoice['net_amount']},{invoice['total']}"
 
 # Update dependencies (2014-09-28 07:49:45 +0000)
+
+# Refactor service layer (2015-02-22 18:04:34 +0000)
