7d50da4ce240efff7a034c9b37b79bab1bd5a02b
Ines Duarte <ines.duarte@example.com>
2015-03-04 00:30:07 +0000

    PROJ-1233: wire up new pricing rules

--- src/notifications/email_templates.py
+++ src/notifications/email_templates.py
@@ -13,3 +13,5 @@ def render_order_confirmation(order):
 # Update documentation (2014-12-13 21:27:13 +0000)
 
 # Refactor service layer (2014-12-26 19:06:54 +0000)
+
+# PROJ-1233: wire up new pricing rules (2015-03-04 00:30:07 +0000)
