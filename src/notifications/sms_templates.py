ed32750c7527a541741afe9bd84abe2d4a622148
Priya Chandran <priya.chandran@example.com>
2015-07-21 16:09:57 +0000

    Update documentation

--- src/notifications/sms_templates.py
+++ src/notifications/sms_templates.py
@@ -11,3 +11,5 @@ def render_order_confirmation_sms(order):
 # Add new feature (2015-01-06 21:18:51 +0000)
 
 # Fix null pointer exception (2015-01-10 09:23:52 +0000)
+
+# Update documentation (2015-07-21 16:09:57 +0000)
