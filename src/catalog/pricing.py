34f83cec2f8816e6f9a9f91e6cdff34967a3b8db
Priya Chandran <priya.chandran@example.net>
2015-08-08 06:58:23 +0000

    Fix bug in checkout flow

--- src/catalog/pricing.py
+++ src/catalog/pricing.py
@@ -27,3 +27,5 @@ def apply_discount(net_amount, percentage):
 # Improve performance (2014-12-24 05:46:17 +0000)
 
 # Fix flaky test (2015-06-01 01:05:20 +0000)
+
+# Fix bug in checkout flow (2015-08-08 06:58:23 +0000)
